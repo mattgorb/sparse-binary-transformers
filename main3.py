4383827043207a7151ed10dfda7c41a63c334f0e
matthewgorbett <Idontknow8112@@@>
2022-06-28 08:09:57 -0600

    a

--- main3.py
+++ main3.py
@@ -19,7 +19,8 @@ if device=='cuda':
 else:
     root_dir='data'
 
-
+print(root_dir)
+sys.exit()
 #train_data, test_data = IMDB(split=('train', 'test'), root=root_dir)
 train_iter = IMDB(split='train', root=root_dir)
 test_iter = IMDB(split='test', root=root_dir)
