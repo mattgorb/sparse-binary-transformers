fdab2dfcfc7cefa9f3a8981bedd61599e8719e67
matthewgorbett <Idontknow8112@@@>
2022-07-02 07:30:25 -0600

    pad mask

--- models/dense_transformer.py
+++ models/dense_transformer.py
@@ -54,7 +54,7 @@ class TransformerModel(nn.Module):
         nn.init.zeros_(self.decoder.bias)
         nn.init.uniform_(self.decoder.weight, -initrange, initrange)
 
-    def forward(self, src, has_src_mask=False, has_pad_mask=True):
+    def forward(self, src, has_src_mask=False, has_pad_mask=False):
         if has_src_mask:
             device = src.device
             if self.src_mask is None or self.src_mask.size(0) != len(src):
