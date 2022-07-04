18d4dbd1b8b4d6628eba81752fd770c632c43292
matthewgorbett <Idontknow8112@@@>
2022-07-03 23:12:54 -0600

    quantization

--- models/layers/sparse_encoder_layer.py
+++ models/layers/sparse_encoder_layer.py
@@ -28,8 +28,10 @@ class SparseTransformerEncoderLayer(nn.Module):
     def forward(self, src, src_mask= None, src_key_padding_mask = None):
         x = src
 
-        x = self.norm1(x + self._sa_block(x, src_mask, src_key_padding_mask))
-        x = self.norm2(x + self._ff_block(x))
+        #x = self.norm1(x + self._sa_block(x, src_mask, src_key_padding_mask))
+        #x = self.norm2(x + self._ff_block(x))
+        x=x + self._sa_block(x, src_mask, src_key_padding_mask)
+        x=x + self._ff_block(x)
 
         return x
 
