b205392452ff940d5841a17df6ed784e3e25cc33
matthewgorbett <Idontknow8112@@@>
2022-07-09 07:08:17 -0600

    gpu

--- main.py
+++ main.py
@@ -127,7 +127,7 @@ def evaluate_memory_size(model, test_dataloader, criterion,):
     #model(text)
     preds=model(text)
     print(preds)
-    macs, params = get_model_complexity_info(model, (512,1), as_strings=True,
+    macs, params = get_model_complexity_info(model, (7206,1), as_strings=True,
                                              print_per_layer_stat=True, verbose=True)
     print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
     print('{:<30}  {:<8}'.format('Number of parameters: ', params))
