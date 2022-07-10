2c57fac3c26e43e3a427c2275743d00d458e3d15
matthewgorbett <Idontknow8112@@@>
2022-07-09 22:44:20 -0600

    gpu

--- metrics/util.py
+++ metrics/util.py
@@ -51,8 +51,8 @@ def get_activations(model, input):
         assert module not in activations, \
             f"{module} already in activations"
         # TODO [0] means first input, not all models have a single input
-        #print(module)
         print(module.__class__)
+        print(module.__class__==nn.MultiheadAttention)
         activations[module] = (input[0].detach().cpu().numpy().copy(),
                                output.detach().cpu().numpy().copy(),)
 
