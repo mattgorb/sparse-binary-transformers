5f76b59d331cb1cd157c58f14967f96009870320
matthewgorbett <Idontknow8112@@@>
2022-07-10 22:15:49 -0600

    evaluation

--- metrics/evaluate.py
+++ metrics/evaluate.py
@@ -65,8 +65,9 @@ def evaluate_flops_memory_size(model, test_dataloader, criterion,train_dataloade
             model, qconfig_spec={torch.nn.LayerNorm}, dtype=torch.qint8,
             inplace=True
         )'''
-        #model.encoder.qconfig = float_qparams_weight_only_qconfig
+
         model.qconfig = torch.quantization.default_qconfig
+        model.encoder.qconfig = float_qparams_weight_only_qconfig
         torch.quantization.prepare(model, inplace=True, )
         torch.quantization.convert(model, inplace=True)
 
