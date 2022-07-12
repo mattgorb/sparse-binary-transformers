2da7771fa1e842ffa6911eb20ef40ece7e2a657d
matthewgorbett <Idontknow8112@@@>
2022-07-12 17:25:48 -0600

    evaluation

--- metrics/evaluate.py
+++ metrics/evaluate.py
@@ -57,12 +57,12 @@ def evaluate_flops_memory_size(model, test_dataloader, criterion,train_dataloade
 
     if args.model_type == 'Dense':
         print('\n\n Running Quantized model...')
-        '''qconfig_dict = {
-            torch.nn.Embedding: float_qparams_weight_only_qconfig,
+        qconfig_dict = {
+            #torch.nn.Embedding: float_qparams_weight_only_qconfig,
             torch.nn.Linear: default_dynamic_qconfig,
             torch.nn.MultiheadAttention: default_dynamic_qconfig
         }
-        quantize_dynamic(model, qconfig_dict, inplace=True,)'''
+        quantize_dynamic(model, qconfig_dict, inplace=True,)
 
         model.qconfig = torch.quantization.default_qconfig
         #model.encoder.qconfig = float_qparams_weight_only_qconfig
