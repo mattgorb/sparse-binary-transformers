40e2eec343d8336ba515ec330c0b34db68734986
matthewgorbett <Idontknow8112@@@>
2022-07-12 20:54:08 -0600

    evaluation

--- metrics/evaluate.py
+++ metrics/evaluate.py
@@ -60,18 +60,18 @@ def evaluate_flops_memory_size(model, test_dataloader, criterion,train_dataloade
         qconfig_dict = {
             torch.nn.Embedding: float_qparams_weight_only_qconfig,
             torch.nn.Linear: default_dynamic_qconfig,
-            torch.nn.MultiheadAttention: default_dynamic_qconfig
+            #torch.nn.MultiheadAttention: default_dynamic_qconfig
         }
         quantize_dynamic(model, qconfig_dict, inplace=True,)
 
         #def unwrap_model(model):
         #print([n for n, _ in model.named_children()])
-        '''layerNorms=[n for n, _ in model.named_modules() if '.norm' in n]
+        layerNorms=[n for n, _ in model.named_modules() if '.norm' in n]
         print(layerNorms)
         for name, layer in model.named_modules():
             if isinstance(layer, nn.LayerNorm):
                 print(name, layer)
-                layer.qconfig=torch.quantization.default_qconfig'''
+                layer.qconfig=torch.quantization.default_qconfig
 
         #model.transformer_encoder.layers[1].norm2.qconfig=torch.quantization.default_qconfig
         #print(layerNorms)
@@ -80,9 +80,9 @@ def evaluate_flops_memory_size(model, test_dataloader, criterion,train_dataloade
         #model.qconfig = torch.quantization.default_qconfig
         #model.encoder.qconfig = float_qparams_weight_only_qconfig
         #torch.quantization.convert()
-        #torch.quantization.prepare(model, inplace=True,)
+        torch.quantization.prepare(model, inplace=True,)
 
-        #torch.quantization.convert(model ,inplace=True,)
+        torch.quantization.convert(model ,inplace=True,)
 
         print(model)
         model.eval()
