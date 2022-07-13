fde5feea2f5d858470f0b9a4b26cb8143601d52e
matthewgorbett <Idontknow8112@@@>
2022-07-12 22:16:09 -0600

    evaluation

--- models/layers/base_transformer_encoder_layer.py
+++ models/layers/base_transformer_encoder_layer.py
@@ -96,7 +96,7 @@ class TransformerEncoderLayer(Module):
             x = self.norm2(x + self._ff_block(x))'''
         x = x + self._sa_block(x, src_mask, src_key_padding_mask)
         x=self.q1(x)
-        print(x.dtype)
+        #print(x.dtype)
         x=self.norm1(x)
         x=self.dq1(x)
         x = x + self._ff_block(x)
