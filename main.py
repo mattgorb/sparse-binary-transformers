78fb2e9685c66468e5fb5e17a6d4d252608e0789
matthewgorbett <Idontknow8112@@@>
2022-07-12 22:25:45 -0600

    evaluation

--- main.py
+++ main.py
@@ -129,6 +129,8 @@ def main():
 
     else:
         model=SBTransformerModel(ntoken=ntokens, ninp=EMBEDDING_DIM, nhead=2, nhid=16, nlayers=2, args=args).to(device)
+    print(model)
+
     freeze_model_weights(model)
     print(f'The model has {count_parameters(model):,} trainable parameters')
 
