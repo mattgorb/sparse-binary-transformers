2fab0d4809be437553c6b15a217e1b5ce6882d8e
matthewgorbett <Idontknow8112@@@>
2022-07-15 11:22:24 -0600

    fix

--- metrics/memory_size.py
+++ metrics/memory_size.py
@@ -80,7 +80,7 @@ def model_size(model,args,quantized=False, as_bits=True):
                 #print(m._get_name())
                 print(f'Weights found for {m._get_name()}')
             else:
-                print(f'No weights for {m}')
+                print(f'No weights for {m._get_name()}')
             continue
             t = np.prod(tensor.shape)
             nz = nonzero(tensor.detach().cpu().numpy())
