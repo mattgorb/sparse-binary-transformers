16c5619e1e2b9a9e832323f83e595858374f1f24
matthewgorbett <Idontknow8112@@@>
2022-07-24 07:58:28 -0600

    dense tf transformer

--- utils/trainer.py
+++ utils/trainer.py
@@ -49,7 +49,7 @@ def test(model, iterator, criterion, device,args, epoch):
             epoch_loss += loss.item()
 
             x=torch.where(label[:,-1]==1,1,0)
-            x=[1 if (label[:,-1]==1 and label[:,-2]==0) else 0 for i in label]
+            x=[1 if (label[i,-1]==1 and label[i,-2]==0) else 0 for i in label]
             if torch.sum(x)>0:
                 print(x)
 
