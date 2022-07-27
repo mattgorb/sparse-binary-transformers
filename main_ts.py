1d5fe0b91f6576c4b5ed8bcd1f33c7d4e72ab1d3
matthewgorbett <Idontknow8112@@@>
2022-07-26 21:25:15 -0600

    anomales

--- main_ts.py
+++ main_ts.py
@@ -81,7 +81,7 @@ def main():
         print(f'\nEpoch {epoch}: ')
         start_time = time.time()
 
-        train_loss = train(model, train_dataloader, optimizer, criterion, device)
+        #train_loss = train(model, train_dataloader, optimizer, criterion, device)
 
         valid_loss = test(model, test_dataloader, criterion, device, args, epoch)
 
