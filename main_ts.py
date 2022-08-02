49c9b09687916ed68973ec600900f016df8f3a46
matthewgorbett <Idontknow8112@@@>
2022-08-02 08:43:04 -0600

    dataset

--- main_ts.py
+++ main_ts.py
@@ -86,7 +86,7 @@ def main():
             start_time = time.time()
 
             train_loss = train(model, train_dataloader, optimizer, criterion, device,args)
-            if epoch==10:
+            if epoch==30:
                 print(f'Entity {ent}')
                 test_loss = test(model, test_dataloader,train_dataloader_ns, criterion, device, args, epoch)
 
