12bd4620a2271c939cf4a4e73ed027f8fa3b90a4
matthewgorbett <Idontknow8112@@@>
2022-08-03 16:25:41 -0600

    images

--- main_ts.py
+++ main_ts.py
@@ -46,7 +46,7 @@ def main():
         args.weight_file = 'weights/' + args.weight_file
     print(root_dir)
 
-    for ent in [11]:
+    for ent in [1]:
         print(f'\n\n\n\n\nEntity {ent}')
         train_dataloader=get_entity_dataset(root_dir, args.batch_size,mode='train',win_size=args.window_size,
                                             dataset=args.dataset, entity=ent, shuffle=True, forecast=args.forecast)
