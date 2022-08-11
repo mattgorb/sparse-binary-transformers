0ffc687217c87853ba28faf7cca1bf4d19e55240
matthewgorbett <Idontknow8112@@@>
2022-08-10 21:01:22 -0600

    teset

--- utils/trainer_anomaly.py
+++ utils/trainer_anomaly.py
@@ -240,10 +240,7 @@ def test(model, test_dataloader,val_dataloader,train_loader, criterion, device, 
         cri = metric * loss
         cri = cri.detach().cpu().numpy()
         attens_energy.extend(cri)
-        test_labels.extend(labels)
-        print(labels.size())
-        print(np)
-        sys.exit()
+        test_labels.extend(labels[:,-1].cpu().numpy())
 
     test_energy = np.array(attens_energy)
     test_labels = np.array(test_labels)
