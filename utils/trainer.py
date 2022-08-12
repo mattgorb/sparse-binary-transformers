b02c81ec5318dd443e7cef7c8d926b26ed61fd2f
matthewgorbett <Idontknow8112@@@>
2022-08-11 18:30:18 -0600

    a

--- utils/trainer.py
+++ utils/trainer.py
@@ -158,8 +158,6 @@ def test(model, iterator,val_iterator, criterion, device,args, entity, epoch):
 
             for i,l,j in zip(index, sample_loss, uniformity_metrics):
                 sample_loss_dict[i.item()]=l.cpu().detach().numpy()
-                print(j.item())
-                sys.exit()
                 sample_attn_dict[i.item()]=j.item()
 
             #first, specifically look at instances with no anomalies at all
