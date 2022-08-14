805297232ded1abd6fe96d432666dda6657c9073
matthewgorbett <Idontknow8112@@@>
2022-08-13 20:59:47 -0600

    a

--- main_ts.py
+++ main_ts.py
@@ -46,7 +46,7 @@ def main():
         weight_file_base = 'weights/' + args.weight_file
 
 
-    for ent in range(4):
+    for ent in range(28):
         weight_file = weight_file_base + f'_entity_{ent}_ds_{args.dataset}_forecast_{args.forecast}_ws_{args.window_size}.pt'
         print(f'\n\n\nEntity {ent}')
         train_dataloader=get_entity_dataset(root_dir, args.batch_size,mode='train',win_size=args.window_size,
