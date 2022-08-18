617c6915ceaa05616b1b5db340a6bad7f5912cc7
matthewgorbett <Idontknow8112@@@>
2022-08-18 13:05:56 -0600

    lr

--- main_ts_classification.py
+++ main_ts_classification.py
@@ -65,7 +65,7 @@ def main():
     freeze_model_weights(model)
     print(f'The model has {count_parameters(model):,} trainable parameters')
 
-    optimizer = optim.RAdam(model.parameters(),lr=1e-3)
+    optimizer = optim.RAdam(model.parameters(),lr=1e-5)
     criterion = nn.CrossEntropyLoss()
     best_loss = 0#float('inf')
 
