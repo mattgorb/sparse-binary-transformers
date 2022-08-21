5ff8fe42ba1e7de8357f32e98b28a6d4430399dc
matthewgorbett <Idontknow8112@@@>
2022-08-21 17:26:22 -0600

    window size update

--- metrics/pot/pot.py
+++ metrics/pot/pot.py
@@ -135,11 +135,12 @@ def pot_eval(init_score, score, label,args, q=1e-5, level=0.02):
 
     while True:
         try:
+            print(lms)
             s = SPOT(q)  # SPOT object
             s.fit(init_score, score)  # data import
 
             s.initialize(level=lms, min_extrema=False, verbose=False)  # initialization step
-            print(lms)
+
         except: lms = lms * 0.999
         else: break
     ret = s.run(dynamic=False)  # run
