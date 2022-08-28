dcec64d2453c8f6675e687688768283002ab4647
matthewgorbett <Idontknow8112@@@>
2022-08-28 09:33:06 -0600

    test

--- models/base/sparse_binary_transformer_ts_cls.py
+++ models/base/sparse_binary_transformer_ts_cls.py
@@ -12,6 +12,8 @@ class TSClsSparseTransformerModel(nn.Module):
         try:
             from models.layers.sparse_encoder import SparseTransformerEncoder
             from models.layers.sparse_encoder_layer import  SparseTransformerEncoderLayer
+            from models.layers.base_transformer_encoder import TransformerEncoder
+            from models.layers.base_transformer_encoder_layer import TransformerEncoderLayer
         except:
             raise ImportError("Had trouble importing transformer modules. ")
         self.model_type = 'Transformer'
@@ -20,14 +22,16 @@ class TSClsSparseTransformerModel(nn.Module):
         self.args=args
         self.pos_encoder = LearnablePositionalEncoding(ninp, )
 
-        encoder_layers = SparseTransformerEncoderLayer(ninp, nhead, nhid, args=self.args)
-        self.transformer_encoder = SparseTransformerEncoder(encoder_layers, nlayers)
+        #encoder_layers = SparseTransformerEncoderLayer(ninp, nhead, nhid, args=self.args)
+        #self.transformer_encoder = SparseTransformerEncoder(encoder_layers, nlayers)
+        encoder_layers = TransformerEncoderLayer(ninp, nhead, nhid,args=self.args,)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
 
-
-        self.embedding = linear_init(input_dim, ninp,args=args,)
+        #self.embedding = linear_init(input_dim, ninp,args=args,)
+        self.embedding = nn.Linear(input_dim, ninp,  )
         self.ninp = ninp
-        self.decoder = linear_init(ninp, classification_labels,bias=False,args=args, )
-
+        #self.decoder = linear_init(ninp, classification_labels,bias=False,args=args, )
+        self.decoder = nn.Linear(ninp, classification_labels,  )
 
         self.init_weights()
 
