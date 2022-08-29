4fa41c5a696011a9fff48acfac503582438f3cda
matthewgorbett <Idontknow8112@@@>
2022-08-28 18:59:13 -0600

    test

--- main_ts.py
+++ main_ts.py
@@ -79,7 +79,7 @@ def main():
         freeze_model_weights(model)
         print(f'The model has {count_parameters(model):,} trainable parameters')
 
-        optimizer = optim.Adam(model.parameters(),lr=1e-4)
+        optimizer = optim.Adam(model.parameters(),lr=args.lr)
         criterion = nn.MSELoss(reduction='none')
         best_loss = float('inf')
 
