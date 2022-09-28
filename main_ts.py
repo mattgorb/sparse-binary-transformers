0ca034518cac5ab2220cc943d3f13bbe2374fef7
matthewgorbett <Idontknow8112@@@>
2022-09-28 09:58:29 -0600

    a

--- main_ts.py
+++ main_ts.py
@@ -91,7 +91,7 @@ def main():
 
         print(f'number of training batches: {train_dataloader.dataset.__len__()/args.batch_size}')
         print(f'number of test batches: {test_dataloader.dataset.__len__()/args.batch_size}')
-        print(f'number of val batches: {val_dataloader.dataset.__len__()/args.batch_size}')
+        #print(f'number of val batches: {val_dataloader.dataset.__len__()/args.batch_size}')
         for epoch in range(args.epochs):
 
 
