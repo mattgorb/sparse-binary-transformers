62b609c6344f1c579448f69e68fa64140bcc7330
matthewgorbett <Idontknow8112@@@>
2022-09-29 21:37:41 -0600

    a

--- utils/trainer.py
+++ utils/trainer.py
@@ -198,7 +198,7 @@ def test_anomaly_detection(model, iterator,val_iterator,train_iterator, criterio
     if result['f1']>best_f1['f1']:
         print(result)
         df = pd.DataFrame({'scores': scores, 'labels':labels})
-        df.to_csv(f'/s/luffy/b/nobackup/mgorb/data/ad_results/scores_{args.dataset}_{result["f1"]}_epoch_{epoch}_.csv')
+        df.to_csv(f'/s/luffy/b/nobackup/mgorb/data/ad_results/scores_{args.dataset}_entity_{entity}.csv')
 
 
     return result,epoch_loss / iterator.dataset.__len__()
