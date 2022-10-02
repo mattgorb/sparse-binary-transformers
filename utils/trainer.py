b76b935fd17ff1dcbda437ef60588a5a979921de
matthewgorbett <Idontknow8112@@@>
2022-10-02 09:35:02 -0600

    a

--- utils/trainer.py
+++ utils/trainer.py
@@ -267,7 +267,7 @@ def metrics(preds, actual,iterator):
 
 
     nonzero_ind=torch.tensor(actual!=0).bool()
-    #print(nonzero_ind)
+    print(nonzero_ind)
     #print(torch.sum(nonzero_ind))
     #print(actual.size())
     #print(len(diffs[nonzero_ind]))
