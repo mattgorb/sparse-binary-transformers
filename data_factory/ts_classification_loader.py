bd81a6dd0894f2acfe2e4037dee302f0b18a6273
matthewgorbett <Idontknow8112@@@>
2022-10-06 11:27:02 -0600

    metrics update

--- data_factory/ts_classification_loader.py
+++ data_factory/ts_classification_loader.py
@@ -306,10 +306,13 @@ def get_classification_ds(dataset,root_dir, args):
     elif dataset == 'JapaneseVowels':
         all_data = TSRegressionArchive(f'{root_dir}/classification/JapaneseVowels', pattern='TRAIN', )
         test_data = TSRegressionArchive(f'{root_dir}/classification/JapaneseVowels', pattern='TEST', n_proc=-1, )
+    elif dataset == 'SelfRegulationSCP1':
+        all_data = TSRegressionArchive(f'{root_dir}/classification/SelfRegulationSCP1', pattern='TRAIN', )
+        test_data = TSRegressionArchive(f'{root_dir}/classification/SelfRegulationSCP1', pattern='TEST', n_proc=-1, )
 
     print(all_data.feature_df.shape)
 
-    if dataset == 'SpokenArabicDigits' or dataset=='Heartbeat' or dataset=='InsectWingbeat' or dataset=='SelfRegulationSCP2' or dataset=='JapaneseVowels':
+    if dataset == 'SpokenArabicDigits' or dataset=='Heartbeat' or dataset=='InsectWingbeat' or dataset=='SelfRegulationSCP1':
         # Note: currently a validation set must exist, either with `val_pattern` or `val_ratio`
         # Using a `val_pattern` means that `val_ratio` == 0 and `test_ratio` == 0
         val_ratio=0.2
