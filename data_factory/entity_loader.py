529f1045b553760d612b25430032ad0cc93c8e68
matthewgorbett <Idontknow8112@@@>
2022-10-12 19:44:44 -0600

    new datasets

--- data_factory/entity_loader.py
+++ data_factory/entity_loader.py
@@ -290,7 +290,7 @@ class Dataset_ETT_hour(Dataset):
         seq_x_mark = self.data_stamp[s_begin:s_end]
         seq_y_mark = self.data_stamp[r_begin:r_end]
 
-        return seq_x, seq_y, seq_x_mark, seq_y_mark
+        return seq_x, seq_y
 
     def __len__(self):
         return len(self.data_x) - self.seq_len - self.pred_len + 1
