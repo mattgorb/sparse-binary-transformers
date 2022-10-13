9dd585a63c362a17138702aec0bcc921df5b48a4
matthewgorbett <Idontknow8112@@@>
2022-10-13 12:44:06 -0600

    new datasets

--- data_factory/entity_loader.py
+++ data_factory/entity_loader.py
@@ -428,14 +428,25 @@ class ForecastDS(object):
             print(self.data.shape)
             print(valid_data.shape)
             print(test_data.shape)
-        elif dataset=='ETTh1':
-            #informer
-            '''train_start = '2016-07-01 00:00:00'
-            train_end = '2017-06-25 23:00:00'
+        elif dataset=='WTH':
+
+            #pyraformer
+            train_start = '2010-01-01 00:00:00'
+            train_end = '2013-03-12 20:00:00'
             valid_start = '2017-06-24 00:00:00'
             valid_end = '2017-10-23 23:00:00'
-            test_start = '2017-10-22 00:00:00'
-            test_end = '2018-02-20 23:00:00'''''
+            test_start = '2013-03-12 20:00:00'
+            test_end = '2013-12-31 23:00:00'
+            data_frame = pd.read_csv(f'{data_path}weather/WTH.csv', index_col=0, parse_dates=True,)
+
+            self.data = data_frame[train_start:train_end].values
+            valid_data = data_frame[valid_start: valid_end].values
+            test_data = data_frame[test_start:test_end].values
+
+            print(self.data.shape)
+            print(valid_data.shape)
+            print(test_data.shape)
+        elif dataset=='ETTh1':
 
             #pyraformer
             train_start = '2016-07-01 00:00:00'
