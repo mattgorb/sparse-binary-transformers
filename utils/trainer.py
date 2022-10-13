8a5d1060a9e51b0d387663a48788b56546a3b753
matthewgorbett <Idontknow8112@@@>
2022-10-12 20:28:28 -0600

    new datasets

--- utils/trainer.py
+++ utils/trainer.py
@@ -257,7 +257,7 @@ def train_forecast(model, iterator, optimizer, criterion, device, args, epoch):
             data[:, -1:, :] = 0
         data = data.to(device)
         data_base = data_base.to(device)
-
+        print(data)
         predictions, _ = model(data.double())
 
         sample_loss = criterion(predictions[:, -1, :], data_base[:, -1, :])
