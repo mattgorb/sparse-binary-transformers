b839ae42bc69bd0601898ef3eecf50d2be23529d
matthewgorbett <Idontknow8112@@@>
2022-10-14 21:11:29 -0600

    new datasets

--- data_factory/entity_loader.py
+++ data_factory/entity_loader.py
@@ -16,6 +16,94 @@ import pickle
 from  sklearn.model_selection import train_test_split
 from data_factory.timefeatures import time_features
 
+
+class MSLSegLoader(object):
+    def __init__(self, data_path, win_size, step, mode="train"):
+        self.mode = mode
+        self.step = step
+        self.win_size = win_size
+        self.scaler = StandardScaler()
+        data = np.load(data_path + "/MSL_train.npy")
+        self.scaler.fit(data)
+        data = self.scaler.transform(data)
+        test_data = np.load(data_path + "/MSL_test.npy")
+        self.test = self.scaler.transform(test_data)
+
+        self.train = data
+        self.val = self.test
+        self.test_labels = np.load(data_path + "/MSL_test_label.npy")
+        print("test:", self.test.shape)
+        print("train:", self.train.shape)
+
+    def __len__(self):
+
+        if self.mode == "train":
+            return (self.train.shape[0] - self.win_size)
+        elif (self.mode == 'val'):
+            return (self.val.shape[0] - self.win_size)
+        elif (self.mode == 'test'):
+            return (self.test.shape[0] - self.win_size)
+        else:
+            return (self.test.shape[0] - self.win_size)
+
+    def __getitem__(self, index):
+        #index = index * self.step
+        if self.mode == "train":
+            return np.float32(self.train[index:index + self.win_size]), np.float32(self.test_labels[0:self.win_size])
+        elif (self.mode == 'val'):
+            return np.float32(self.val[index:index + self.win_size]), np.float32(self.test_labels[0:self.win_size]), index
+        elif (self.mode == 'test'):
+            return np.float32(self.test[index:index + self.win_size]), np.float32(
+                self.test_labels[index:index + self.win_size]), index
+        else:
+            return np.float32(self.test[
+                              index // self.step * self.win_size:index // self.step * self.win_size + self.win_size]), np.float32(
+                self.test_labels[index // self.step * self.win_size:index // self.step * self.win_size + self.win_size])
+
+
+class SMAPSegLoader(object):
+    def __init__(self, data_path, win_size, step, mode="train"):
+        self.mode = mode
+        self.step = step
+        self.win_size = win_size
+        self.scaler = StandardScaler()
+        data = np.load(data_path + "/SMAP_train.npy")
+        self.scaler.fit(data)
+        data = self.scaler.transform(data)
+        test_data = np.load(data_path + "/SMAP_test.npy")
+        self.test = self.scaler.transform(test_data)
+
+        self.train = data
+        self.val = self.test
+        self.test_labels = np.load(data_path + "/SMAP_test_label.npy")
+        print("test:", self.test.shape)
+        print("train:", self.train.shape)
+
+    def __len__(self):
+
+        if self.mode == "train":
+            return (self.train.shape[0] - self.win_size)
+        elif (self.mode == 'val'):
+            return (self.val.shape[0] - self.win_size)
+        elif (self.mode == 'test'):
+            return (self.test.shape[0] - self.win_size)
+        else:
+            return (self.test.shape[0] - self.win_size)
+
+    def __getitem__(self, index):
+        #index = index * self.step
+        if self.mode == "train":
+            return np.float32(self.train[index:index + self.win_size]), np.float32(self.test_labels[0:self.win_size])
+        elif (self.mode == 'val'):
+            return np.float32(self.val[index:index + self.win_size]), np.float32(self.test_labels[0:self.win_size]), index
+        elif (self.mode == 'test'):
+            return np.float32(self.test[index:index + self.win_size]), np.float32(
+                self.test_labels[index:index + self.win_size]), index
+        else:
+            return np.float32(self.test[
+                              index // self.step * self.win_size:index // self.step * self.win_size + self.win_size]), np.float32(
+                self.test_labels[index // self.step * self.win_size:index // self.step * self.win_size + self.win_size])
+
 class SMDSegLoader(object):
     def __init__(self, data_path, win_size, step, mode="train"):
         self.mode = mode
@@ -542,18 +630,18 @@ def get_entity_dataset(data_path, batch_size, win_size=100, step=100, mode='trai
         dataset=SMDSegLoader(f'{data_path}/SMD/', win_size, step, mode=mode)
         #dataset = SMD(data_path,entities[entity], win_size, step, mode, forecast)
     elif dataset == 'SMAP':
-        entities=pd.read_csv(f'{data_path}/SMAP_MSL/labeled_anomalies.csv')
-        entities=entities[entities['spacecraft']=='SMAP']
-        entities=entities['chan_id'].values
-        print(f'Dataset: {entities[entity]}')
-        dataset = SMAP_MSL(data_path, entities[entity], win_size, step, mode, forecast)
+        #entities=pd.read_csv(f'{data_path}/SMAP_MSL/labeled_anomalies.csv')
+        #entities=entities[entities['spacecraft']=='SMAP']
+        #entities=entities['chan_id'].values
+        #print(f'Dataset: {entities[entity]}')
+        dataset = SMAPSegLoader(f'{data_path}/SMAP/', win_size, step, mode=mode)
         #print(entities)
     elif dataset == 'MSL':
-        entities = pd.read_csv(f'{data_path}/SMAP_MSL/labeled_anomalies.csv')
-        entities = entities[entities['spacecraft'] == 'MSL']
-        entities = entities['chan_id'].values
-        print(f'Dataset: {entities[entity]}')
-        dataset = SMAP_MSL(data_path,entities[entity], win_size, step, mode, forecast)
+        #entities = pd.read_csv(f'{data_path}/SMAP_MSL/labeled_anomalies.csv')
+        #entities = entities[entities['spacecraft'] == 'MSL']
+        #entities = entities['chan_id'].values
+        #print(f'Dataset: {entities[entity]}')
+        dataset = MSLSegLoader(f'{data_path}/MSL/', win_size, step, mode=mode)
     elif dataset == 'electricity' :
         #dataset = electTestDataset(data_path+'electricity/', dataset,1,mode )
         dataset = ForecastDS(data_path, win_size, step, mode, dataset)
