f4e500e3fdfb8ef944b873c34fc3b9d184d6cf8f
matthewgorbett <Idontknow8112@@@>
2022-10-16 22:43:54 -0600

    evaluation meetricss

--- metrics/memory_size.py
+++ metrics/memory_size.py
@@ -72,8 +72,8 @@ def model_size(model,args,quantized=False, as_bits=True):
                     print(f'{name} continuing...')
                     continue
             t = np.prod(tensor.shape)
-            print(f'Weights found for {name}')
-            #print(t)
+            print(f'Weights found for {name}, {t}')
+            print(t)
             nz = nonzero(tensor.detach().cpu().numpy())
             bits = dtype2bits[tensor.dtype]
             params_dict['total_bits']+=(bits*t)
