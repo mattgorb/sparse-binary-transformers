83589030012728df10c40ab4f6f866572be001ac
matthewgorbett <Idontknow8112@@@>
2022-10-17 08:34:05 -0600

    evaluation meetricss

--- main_ts.py
+++ main_ts.py
@@ -35,10 +35,11 @@ def epoch_time(start_time, end_time):
     elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
     return elapsed_mins, elapsed_secs
 
-def rerandomize_model(model, args):
+def print_model(model, args):
     for n, m in model.named_modules():
-        if hasattr(m, "weight") and m.weight is not None:
-            print(f'{n}: {m.weight.numel()}')
+        print(f'{n}')
+        #if hasattr(m, "weight") and m.weight is not None:
+            #print(f'{n}: {m.weight.numel()}')
 
 def main():
     device = torch.device(f'cuda:{args.gpu}' if torch.cuda.is_available() else 'cpu')
@@ -85,8 +86,8 @@ def main():
     else:
         print("Invalid")
         sys.exit()
-    rerandomize_model(model, args)
-    #sys.exit()
+    print_model(model, args)
+    sys.exit()
     freeze_model_weights(model)
     print(f'The model has {count_parameters(model):,} trainable parameters')
 
