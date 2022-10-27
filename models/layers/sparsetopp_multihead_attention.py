9ca32d1d013316e8ed8e84ce45379993fee81230
matthewgorbett <Idontknow8112@@@>
2022-10-27 13:38:54 -0600

    ablation

--- models/layers/sparsetopp_multihead_attention.py
+++ models/layers/sparsetopp_multihead_attention.py
@@ -442,13 +442,10 @@ class SparseTopPMultiheadAttention(nn.MultiheadAttention):
         if self.softmax_mask is not None:
             #attn_output_weights.masked_fill_(self.softmax_mask , float('-inf'))
             attn_output_weights+=self.softmax_mask#.repeat(attn_output_weights.size(0),1,1)
-        print('here')
-        print(self.softmax_mask)
-        print(attn_output_weights)
+
         attn_output_weights = nnF.softmax(
             attn_output_weights, dim=-1)
-        print(attn_output_weights)
-        sys.exit()
+
         attn_output_weights = nnF.dropout(attn_output_weights, p=self.dropout, training=self.training)
 
         attn_output = torch.bmm(attn_output_weights, v)
