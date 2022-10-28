c69c406d119022ccaa7a2b17505e6b71244307e1
matthewgorbett <Idontknow8112@@@>
2022-10-27 20:33:45 -0600

    ablation

--- run_classification.sh
+++ run_classification.sh
@@ -4,6 +4,7 @@
 nohup python3 -u main_ts_classification.py --config=configs/sparse_ts_classification_arabic.yaml --gpu=2 --lin_prune_rate=0.5 --attention_prune_rate=0.5> ~/results_classification/1.txt 2>&1 &
 nohup python3 -u main_ts_classification.py --config=configs/sparse_ts_classification_fd.yaml --gpu=3 --lin_prune_rate=0.5 --attention_prune_rate=0.5> ~/results_classification/2.txt 2>&1 &
 nohup python3 -u main_ts_classification.py --config=configs/sparse_ts_classification_hb.yaml --gpu=4 --lin_prune_rate=0.5 --attention_prune_rate=0.5> ~/results_classification/3.txt 2>&1 &
+
 nohup python3 -u main_ts_classification.py --config=configs/sparse_ts_classification_insect.yaml --gpu=5 --lin_prune_rate=0.5 --attention_prune_rate=0.5> ~/results_classification/4.txt 2>&1 &
 nohup python3 -u main_ts_classification.py --config=configs/sparse_ts_classification_japan.yaml --gpu=6 --lin_prune_rate=0.5 --attention_prune_rate=0.5> ~/results_classification/5.txt 2>&1 &
 
