27daf83499d4a4ceef551af353897f6291302feb
matthewgorbett <Idontknow8112@@@>
2023-04-11 09:10:29 -0600

    newest

--- models/layers/sparsetopp_multihead_attention.py
+++ models/layers/sparsetopp_multihead_attention.py
@@ -312,8 +312,8 @@ class SparseTopPMultiheadAttention(nn.MultiheadAttention):
             #print(q_sort_val[:,prune_size:].size())
             print(q_sort_val.dtype)
             print(torch.zeros_like(q.view(-1,q.size(0)*q.size(2) )).dtype)
-            print(q_sort_val[:,:prune_size ].int64().dtype)
-            q.view(-1,q.size(0)*q.size(2) ).scatter(1, q_sort_val[:,:prune_size ].int64(),torch.zeros_like(q.view(-1,q.size(0)*q.size(2) )) )
+            print(q_sort_val[:,:prune_size ].long().dtype)
+            q.view(-1,q.size(0)*q.size(2) ).scatter(1, q_sort_val[:,:prune_size ].long(),torch.zeros_like(q.view(-1,q.size(0)*q.size(2) )) )
             #q.view(-1,q.size(0)*q.size(2) )[q_sort_ind[0,0]] = 0
 
 
