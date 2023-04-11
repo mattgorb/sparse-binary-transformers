9f053695c54e4436c77a3526fc5db5a8ba99d7bd
matthewgorbett <Idontknow8112@@@>
2023-04-11 09:36:24 -0600

    newest

--- run_classification.sh
+++ run_classification.sh
@@ -24,10 +24,10 @@ nohup python3 -u main_ts_classification.py --config=configs/sparse_ts_classifica
 
 
 
-nohup python3 -u main_ts_classification.py --config=configs/sparse_ts_classification_arabic.yaml --gpu=4 --lin_prune_rate=0.5 --attention_prune_rate=0.5 --dmodel=64 --ablation=True> ~/results_classification/1.txt 2>&1 &
-nohup python3 -u main_ts_classification.py --config=configs/sparse_ts_classification_fd.yaml --gpu=4 --lin_prune_rate=0.5 --attention_prune_rate=0.5 --dmodel=256 --ablation=True> ~/results_classification/2.txt 2>&1 &
-nohup python3 -u main_ts_classification.py --config=configs/sparse_ts_classification_hb.yaml --gpu=4 --lin_prune_rate=0.5 --attention_prune_rate=0.5 --epochs=64 --dmodel=64 --epochs=50 --ablation=True> ~/results_classification/3.txt 2>&1 &
-nohup python3 -u main_ts_classification.py --config=configs/sparse_ts_classification_insect.yaml --gpu=4 --lin_prune_rate=0.5 --attention_prune_rate=0.5 --dmodel=256 --ablation=True> ~/results_classification/4.txt 2>&1 &
+nohup python3 -u main_ts_classification.py --config=configs/sparse_ts_classification_arabic.yaml --gpu=0 --lin_prune_rate=0.5 --attention_prune_rate=0.5 --dmodel=64 --ablation=True> ~/results_classification/1.txt 2>&1 &
+nohup python3 -u main_ts_classification.py --config=configs/sparse_ts_classification_fd.yaml --gpu=0 --lin_prune_rate=0.5 --attention_prune_rate=0.5 --dmodel=256 --ablation=True> ~/results_classification/2.txt 2>&1 &
+nohup python3 -u main_ts_classification.py --config=configs/sparse_ts_classification_hb.yaml --gpu=2 --lin_prune_rate=0.5 --attention_prune_rate=0.5 --epochs=64 --dmodel=64 --epochs=50 --ablation=True> ~/results_classification/3.txt 2>&1 &
+nohup python3 -u main_ts_classification.py --config=configs/sparse_ts_classification_insect.yaml --gpu=3 --lin_prune_rate=0.5 --attention_prune_rate=0.5 --dmodel=256 --ablation=True> ~/results_classification/4.txt 2>&1 &
 nohup python3 -u main_ts_classification.py --config=configs/sparse_ts_classification_japan.yaml --gpu=4 --lin_prune_rate=0.5 --attention_prune_rate=0.5 --ablation=True> ~/results_classification/5.txt 2>&1 &
 
 
