75c9e9bae882dd2111db525d05669828c2b5997e
matthewgorbett <Idontknow8112@@@>
2023-04-10 21:56:40 -0600

    newest

--- models/layers/sparsetopp_multihead_attention.py
+++ models/layers/sparsetopp_multihead_attention.py
@@ -308,17 +308,14 @@ class SparseTopPMultiheadAttention(nn.MultiheadAttention):
 
             q.view(-1,q.size(0)*q.size(2) )[q_sort_ind[:,prune_size:]] = 0
 
-            #k_sort_val, k_sort_ind = torch.sort(k.abs().view(-1,k.size(0)*k.size(2)),dim=-1, descending=True)
-            '''print(k.view(-1,k.size(0)*k.size(2)).size())
-            print(k_sort_ind[:,prune_size:].size())
-            print(k.size())
-            print("HERE")
-            print(k_sort_ind[:,prune_size:].size())
-            print(k_sort_ind.size() )
-            print(k_sort_ind)'''
-            print(q.size())
-            print(q)
+            print(q.view(-1,q.size(0)*q.size(2) ).size())
+            print(q_sort_ind[:,prune_size:].size())
             sys.exit()
+
+
+
+            k_sort_val, k_sort_ind = torch.sort(k.abs().view(-1,k.size(0)*k.size(2)),dim=-1, descending=True)
+
             k.view(-1,k.size(0)*k.size(2))[k_sort_ind[:,prune_size:]] = 0
 
             sys.exit()
